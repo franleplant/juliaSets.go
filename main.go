699bb8c3c55b680fa326c89fa860d9d00218391d
Fran Guijarro <franleplant@gmail.com>
2015-02-01 18:56:36 -0300

    keep refactoring. Add examples

--- main.go
+++ main.go
@@ -7,22 +7,34 @@ import (
 )
 
 const (
-	N       = 500
+	N       = 1000
 	MIN_x   = float64(-1.0)
-	c1      = -0.4 + 0.6i //nice
 	c2      = -0.285 + 0.1i
-	c3      = (1 - 1.618033987) + 0.0i //Golden ratio
-	c4      = -0.8 + 0.156i            //nice
-	c5      = 0.279 + 0.0i             //really nice
-	c6      = 0 + 0i                   //circle
-	c7      = -2 + 0i
 	PHASE   = 150.0
-	K_COLOR = 5.0
+	K_COLOR = 4.0
 )
 
 func fz1(z complex128) complex128 {
 	return cmplx.Pow(z, 2) + 0.279 + 0.0i
 }
+
+// Golden ratio
+func fz2(z complex128) complex128 {
+	return cmplx.Pow(z, 2) + (1 - 1.618033987) + 0.0i
+}
+
+func fz3(z complex128) complex128 {
+	return cmplx.Pow(z, 2) - 0.8 + 0.156i
+}
+
+func fz4(z complex128) complex128 {
+	return cmplx.Pow(z, 2) - 0.4 + 0.6i
+}
+
+func fz5(z complex128) complex128 {
+	return cmplx.Pow(z, 2) - 0.285 + 0.1i
+}
+
 func main() {
 	getColor := utils.GetColorFactory(K_COLOR, PHASE)
 	alg.EscapeTime(fz1, getColor, N, MIN_x)
--- utils/GetColorFactory.go
+++ utils/GetColorFactory.go
@@ -7,6 +7,9 @@ import (
 
 type GetColorType func(count int) (r, g, b uint8)
 
+// COLOR_K is a proportional constant for the color, the higher the more difference there will be between two
+// points with the same count
+// COLOR_PHASE selects from where the color palette will start
 func GetColorFactory(COLOR_K float64, COLOR_PHASE float64) GetColorType {
 	//TODO: if count is 0 then return black and if is MAX_ITER then return white (or viceversa)
 	return func(count int) (r, g, b uint8) {
