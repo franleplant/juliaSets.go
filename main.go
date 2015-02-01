795000bf55d2411f16a260344ed236fdf1f0756e
Fran Guijarro <franleplant@gmail.com>
2015-02-01 18:30:50 -0300

    keep refactoring

--- main.go
+++ main.go
@@ -3,11 +3,8 @@
 package main
 
 import (
-	"fmt"
+	"github.com/franleplant/juliaSets/alg"
 	"github.com/franleplant/juliaSets/utils"
-	"image"
-	"image/color"
-	"image/draw"
 	"math/cmplx"
 )
 
@@ -31,63 +28,5 @@ func fz1(z complex128) complex128 {
 }
 func main() {
 	getColor := utils.GetColorFactory(K_COLOR, PHASE)
-	escapeTime(fz1, getColor, N, MIN_x)
-}
-
-// Sufficiently big enough matrix
-type CMatrix [5000][5000]complex128
-
-type fzType func(z complex128) complex128
-
-var fzi = fz1
-
-var m CMatrix
-var z *complex128
-
-func escapeTime(fz fzType, getColor utils.GetColorType, N int, MIN_x float64) {
-	const (
-		MAX_ITERATIONS = 3000
-		ER             = 1
-	)
-
-	var count int
-	var r, g, b uint8
-	var (
-		M      = N
-		MAX_x  = -MIN_x
-		MIN_y  = MIN_x
-		MAX_y  = MAX_x
-		STEP_x = (MAX_x - MIN_y) / float64(M)
-		STEP_y = (MAX_y - MIN_y) / float64(N)
-	)
-
-	img := image.NewRGBA(image.Rect(0, 0, M, N))
-
-	for i := 0; i < N; i++ {
-		for j := 0; j < M; j++ {
-			z = &m[i][j]
-			*z = complex(MIN_x+float64(j)*STEP_x, MAX_y-float64(i)*STEP_y)
-			//fmt.Println(*z)
-
-			for count = 0; count < MAX_ITERATIONS; count++ {
-				//*z = cmplx.Pow(*z, 2) + c
-				*z = fzi(*z)
-				if cmplx.Abs(*z) > ER {
-					//fmt.Printf("count %v color %v ", count, (math.Mod(PHASE*float64(count), 255.0))/255.0)
-					break
-				}
-			}
-
-			if count == MAX_ITERATIONS {
-				fmt.Println("Max Iterations reached")
-			}
-
-			r, g, b = getColor(count)
-			//fmt.Printf("r %v g %v b %v \n", r, g, b)
-
-			draw.Draw(img, image.Rect(j, i, j+1, i+1), &image.Uniform{color.RGBA{r, g, b, 255}}, image.ZP, draw.Src)
-		}
-	}
-
-	utils.SaveImg(img)
+	alg.EscapeTime(fz1, getColor, N, MIN_x)
 }
